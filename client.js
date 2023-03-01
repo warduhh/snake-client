f977f63414348198b17b0daee910065c6175322a
Warda Nur <warduhh@gmail.com>
2023-03-01 02:32:32 +0000

    Created test move commands

--- client.js
+++ client.js
@@ -1,26 +1,42 @@
+const { time } = require("console");
 const net = require("net");
 // establishes a connection with the game server
-
- connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "10.0.2.15", // IP address here,
     port: "50541" // PORT number here,
   });
-conn.on("connect", () => {
-console.log("succesfully conected to server")
+
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: WAR');
 
 
-});
 
-conn.on("data",(data) => {
-console.log("server says" , data)
+   
+  setTimeout(() => {
+    setInterval(() => {
+     conn.write("Move: up")
+      
+    }, 50)
+  }, 800) 
+  
 
-});
+  });
+
+  conn.on("data",(data) => {
+     console.log("server says" , data);
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  conn.write("Move: up")
+ 
+  
   return conn;
 };
 
 console.log("Connecting ...");
 connect();
+
+
+module.exports = {connect};
