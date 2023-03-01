69cb65f70e53be1d126704e92998ad524359b8c8
Warda Nur <warduhh@gmail.com>
2023-03-01 20:58:44 +0000

    Add move commands for the snake

--- input.js
+++ input.js
@@ -1,19 +1,39 @@
-const setupInput = function () {
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data", handleUserInput)
   return stdin;
 };
-const handleUserInput = function () {
-  stdin.on("data", handleUserInput);
+
+const handleUserInput = function (key) {
+
+  // ctrl-c (to terminate the game)
   if (key === '\u0003') {
-    process.stdout.write('Terminated');
-    process.stdout.write('\n');
+  
     process.exit();
   }
-  handleUserInput();
-  return stdin;
-  };
- 
-  module.exports = {setupInput};
+
+  if (key === 'w') {
+   
+    connection.write('Move: up')
+  }
+  if (key === 'a') {
+    connection.write('Move: left')
+  }
+  if (key === 's') {
+    connection.write('Move: down')
+  }
+  if (key === 'd') {
+    connection.write('Move: right')
+  }
+
+
+
+
+
+};
+module.exports = { setupInput };
