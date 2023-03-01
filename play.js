414cfa6b5ca1c7a48e37a7524ad25883a200c6bc
Warda Nur <warduhh@gmail.com>
2023-03-01 04:31:16 +0000

    Refracted file

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-const handleUserInput = function () {
-  stdin.on("data", handleUserInput);
-  if (key === '\u0003') {
-    process.stdout.write('Terminated');
-    process.stdout.write('\n');
-    process.exit();
-  }
-  handleUserInput();
-  return stdin;
-  };
- 
+console.log("Connecting ...");
+connect();
+
+setupInput();
