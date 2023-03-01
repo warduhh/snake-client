b0d2013b3363b15852cbd892f1a4c397a53794a0
Warda Nur <warduhh@gmail.com>
2023-03-01 21:42:49 +0000

    Create constants file to move networking info

--- client.js
+++ client.js
@@ -1,20 +1,21 @@
 const { time } = require("console");
+const { IP, PORT } = require("./constants");
 const net = require("net");
 const connect = function() {
   const conn = net.createConnection({
-    host: "localhost",
-    port: "50541" 
+    host: IP,
+    port: PORT
   });
 
   conn.on("connect", () => {
     console.log('Successfully connected to game server');
-    conn.write('Name: WAR');
-
-
+    conn.write('Name: WAR')
+    
   });
 
   conn.on("data",(data) => {
      console.log("server says" , data);
+    
   });
 
   conn.setEncoding("utf8");
